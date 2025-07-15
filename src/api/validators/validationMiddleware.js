ef438777b47aadb1df4b54c26c0997a10e1d2bb5
Hhassan5 <hhassan287@aucegypt.edu>
2025-07-15 04:13:56 -0400

    Adding temporary changes to Github

---
+++ src/api/validators/validationMiddleware.js
@@ -0,0 +1,19 @@
+const { registerSchema, loginSchema } = require('./userValidator');
+
+const validateRegistration = (req, res, next) => {
+  const { error } = registerSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
+const validateLogin = (req, res, next) => {
+  const { error } = loginSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
+module.exports = { validateRegistration, validateLogin };
